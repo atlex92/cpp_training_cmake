c52fed8390414599c242c2cef4bf94dc95b32f87
RouckWind <infinite11145@gmail.com>
2023-01-30 13:30:49 +0200

    On branch dev/p.dvorcov  Change test cases 	modified:   tests/main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,34 +4,34 @@
 TEST(Task1004, Task1004) {
 	Task1004 task;
 	//  not initialized, zero length num array
-	EXPECT_EQ(0u, task.GetAnswer());
+	EXPECT_EQ(0, task.GetAnswer());
 
-	task.Setup({ 1,1,1 }, 0u);
-	EXPECT_EQ(3u, task.GetAnswer());
+	task.Setup({ 1,1,1 }, 0);
+	EXPECT_EQ(3, task.GetAnswer());
 
-	task.Setup({ 1,0,1 }, 0u);
-	EXPECT_EQ(1u, task.GetAnswer());
+	task.Setup({ 1,0,1 }, 0);
+	EXPECT_EQ(1, task.GetAnswer());
 
-	task.Setup({ 0,0,0,0,0 }, 3u);
-	EXPECT_EQ(3u, task.GetAnswer());
+	task.Setup({ 0,0,0,0,0 }, 3);
+	EXPECT_EQ(3, task.GetAnswer());
 
-	task.Setup({ 0,0,0,0,0 }, 5u);
-	EXPECT_EQ(5u, task.GetAnswer());
+	task.Setup({ 0,0,0,0,0 }, 5);
+	EXPECT_EQ(5, task.GetAnswer());
 
 	//  example from leetcode
 	task.Setup({ 1,1,1,0,0,0,1,1,1,1,0 }, 2u);
-	EXPECT_EQ(6u, task.GetAnswer());
+	EXPECT_EQ(6, task.GetAnswer());
 
 	//  example from leetcode
 	task.Setup({ 0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1 }, 3u);
-	EXPECT_EQ(10u, task.GetAnswer());
+	EXPECT_EQ(10, task.GetAnswer());
 
 	//  example from my wife teacher Math Nadezhda
 	task.Setup({ 1,1,1,0,1,1,0,0,0,0,1,1,1,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,1,1,1,1,0,1,1,1,1,0,0,1,1,1,1 }, 3u);
-	EXPECT_EQ(15u, task.GetAnswer());
+	EXPECT_EQ(15, task.GetAnswer());
 }
 
-TEST(Task209, Task209) {
+/*TEST(Task209, Task209) {
 	Task209 task;
 
 	//  not initialized, zero length num array
@@ -48,7 +48,7 @@ TEST(Task209, Task209) {
 	//  example from leetcode
 	task.Setup({ 1,1,1,1,1,1,1,1 }, 11u);
 	EXPECT_EQ(0u, task.GetAnswer());
-}
+}*/
 
 int main(int argc, char** argv) {
 
