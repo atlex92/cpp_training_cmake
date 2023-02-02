ea974bab0edc15d8390a17a62893915204e194b8
RouckWind <infinite11145@gmail.com>
2023-02-02 17:04:14 +0200

    On branch dev/p.dv  Minor changes task

--- src/tasks/include/tasks.h
+++ src/tasks/include/tasks.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include <limits>
+#include <cstddef>
+
 struct Task1004 {
 /**
  * Дан массив произвольного размера, содержащий цифры '0' и '1', а также число k.
@@ -15,25 +17,25 @@ struct Task1004 {
     }
 
     int GetAnswer() const {
-        int left{0};
-        int right{0};
+        int left_border{0};
+        int right_border{0};
         int flips_left{k_};
         int res = std::numeric_limits<int>::min();
 
         if (nums_.size() == 0)
             return 0;
 
-        for (right; right < nums_.size(); ++right) {
-            if (nums_[right] == 0)
+        for (right_border; right_border < nums_.size(); ++right_border) {
+            if (nums_[right_border] == 0)
                 flips_left--;
             while (flips_left < 0) {
-                if (nums_[left] == 0)
+                if (nums_[left_border] == 0)
                     flips_left++;
-                left++;
+                left_border++;
             }
-            res = std::max(res, right - left + 1);
+            res = std::max(res, right_border - left_border + 1);
         }
-        return res; 
+       return res; 
     }
     
 private:
@@ -41,13 +43,13 @@ private:
     int k_{0};
 };
 
-/*struct Task209 {
+struct Task209 {
 /**
  * Дан массив с положительными целыми числами произвольного размера, а также число целевое значение желаемой суммы summ.
  * Необходимо посчитать минимальную длину последовательности в массиве, сумма элементов в которой будет >= summ.
  * Если такой последовательности в массиве нет, нужно вернуть 0.
  * https://leetcode.com/problems/minimum-size-subarray-sum/
- 
+**/ 
     void Setup(const std::vector<int>& input_num, const size_t summ) {
         nums_ = input_num;
         summ_ = summ;
@@ -60,4 +62,4 @@ private:
 private:
     std::vector<int> nums_;
     size_t summ_{0u};
-};*/
+};
